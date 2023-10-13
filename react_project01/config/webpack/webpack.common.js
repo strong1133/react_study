a3225cb0af0ddcde91a174697d1a14123f2907f4
strong3879@gmail.com <seokjin.jeong@timf.co.kr>
2023-10-13 17:52:16 +0900

    base set

---
+++ react_project01/config/webpack/webpack.common.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  // watch: true,
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    modules: [path.resolve(__dirname, "../../node_modules"), "node_modules"],
+    alias: {
+      "@": "./",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "../../dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/views/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
---
+++ react_project01/src/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1>
+                React Sample
+            </h1>
+        </>
+    )
+
+};
+
+export default App;
